9c36b8b96db721f0d19ef2c72ef8901b84040bf9
hg233-remao <2690976779@qq.com>
2026-03-14 15:42:16 +0800

    1

--- obj/Debug/net8.0/BO.AssemblyInfo.cs
+++ obj/Debug/net8.0/BO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f921e94123bbb292ab8fb96528935df52f06ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d4cd9d1dad853f31cb1815439a5478b4e5c337")]
 [assembly: System.Reflection.AssemblyProductAttribute("BO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
